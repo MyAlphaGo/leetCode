fcb29d30d8886554c47403a7b95ee3be89c84ad6
AlphaGo丶忄 <49247161+MyAlphaGo@users.noreply.github.com>
2022-01-19 16:39:21 +0800

    回溯：括号生成

--- backtrack/括号生成.js
+++ backtrack/括号生成.js
@@ -1,9 +1,12 @@
-### 22. 括号生成(https://leetcode-cn.com/problems/generate-parentheses/)
-#### 描述
-数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+// 括号生成
+// 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/generate-parentheses
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 
-```js
+
 /**
  * @param {number} n
  * @return {string[]}
@@ -25,4 +28,4 @@ var generateParenthesis = function (n) {
     dp(n, 0, '');
     return result;
 };
-```
+
