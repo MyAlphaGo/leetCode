95d088e86d77216b515c0dfd6e87e1011c2d74dd
AlphaGo丶忄 <49247161+MyAlphaGo@users.noreply.github.com>
2022-01-19 16:41:45 +0800

    回溯：电话号码的字母组合

--- backtrack/电话号码的字母组合.js
+++ backtrack/电话号码的字母组合.js
@@ -1,13 +1,13 @@
-### 17. 电话号码的字母组合(https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/)
-#### 描述
-  给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
-  给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
-  
-```js
-/**
- * @param {string} digits
- * @return {string[]}
- */
+// 17. 电话号码的字母组合
+
+// 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+// 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
 var letterCombinations = function (digits) {
     if(digits.length === 0) {
         return []
@@ -40,4 +40,4 @@ var letterCombinations = function (digits) {
     dp(digits, '')
     return Object.keys(unique)
 };
-```
+
