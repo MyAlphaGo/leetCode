e665ab4a9e3ad410004b74cdabef27df4181b716
AlphaGo丶忄 <49247161+MyAlphaGo@users.noreply.github.com>
2022-01-19 16:52:39 +0800

    排序：最小时间差

--- sort/最小时间差.js
+++ sort/最小时间差.js
@@ -1,22 +1,10 @@
-### 539. 最小时间差(https://leetcode-cn.com/problems/minimum-time-difference/)
-#### 描述            
-给定一个 24 小时制（小时:分钟 "HH:MM"）的时间列表，找出列表中任意两个时间的最小时间差并以分钟数表示。
+// 539. 最小时间差
+// 给定一个 24 小时制（小时:分钟 "HH:MM"）的时间列表，找出列表中任意两个时间的最小时间差并以分钟数表示。
 
-#### 示例 1：
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/minimum-time-difference
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
-输入：timePoints = ["23:59","00:00"]
-输出：1
-示例 2：
-
-输入：timePoints = ["00:00","23:59","00:00"]
-输出：0
- 
-
-#### 提示：
-2 <= timePoints.length <= 2 * 104
-timePoints[i] 格式为 "HH:MM"
-
-```js
 /**
  * @param {string[]} timePoints
  * @return {number}
@@ -36,4 +24,4 @@ var findMinDifference = function (timePoints) {
     return Math.min(minValue, Math.abs((timePoints[0] + MAX_VALUE) - timePoints[len - 1]))
 
 };
-```
+
